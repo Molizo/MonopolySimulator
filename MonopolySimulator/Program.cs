64f4c6e78d3cf5ada3733e4c49c3c4f9f799611d
Visoiu Mihnea Theodor <visoiu.mihnea@gmail.com>
2018-08-28 19:00:35 +0300

    Added game properties loading

--- MonopolySimulator/Program.cs
+++ MonopolySimulator/Program.cs
@@ -1,30 +1,72 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace MonopolySimulator
 {
+    public class Property
+    {
+        public string id { set; get; }
+        public string name { set; get; }
+        public string type { set; get; }
+        public string payout { set; get; }
+    }
+
     internal class Program
     {
+        private static readonly List<Property> boardPropertys = new List<Property>();
+        private static ulong turns = 0, games = 0;
+
         private static void Main(string[] args)
         {
-            ulong turns = 0, games = 0;
-            string title = "+-------------------------------------------------------+" + System.Environment.NewLine +
-                           "|  Monopoly Turn Simulator by Visoiu Mihnea Theodor     |" + System.Environment.NewLine +
-                           "|                                                       |" + System.Environment.NewLine +
-                           "|  This program simulates the turns made by a Monopoly  |" + System.Environment.NewLine +
-                           "|  player in order to find the most visited location    |" + System.Environment.NewLine +
-                           "|                                                       |" + System.Environment.NewLine +
-                           "|  The developer is not associated with Hasbro Ent.     |" + System.Environment.NewLine +
-                           "+-------------------------------------------------------+";
-            Console.WriteLine(title);
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("The board configuration file is located in the same folder");
-            Console.WriteLine("as the executable. It is named boardConfig.xlsx");
-            Console.WriteLine("DO NOT ALTER THE HEADERS IN boardConfig.xlsx");
-            Console.WriteLine("The config file may be opened with a spreadsheet program.");
-            Console.WriteLine();
-            Console.WriteLine();
+            showTitle();
+            readInput();
+            readBoardConfig();
 
+            Console.WriteLine("Program terminated...");
+            Console.Read();
+        }
+
+        private static void readBoardConfig()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Reading configuration file: ");
+            Console.WriteLine("Properties:");
+            try
+            {
+                FileInfo fi = new FileInfo(@".\boardConfig.xlsx");
+                using (ExcelPackage workbook = new ExcelPackage(fi))
+                {
+                    ExcelWorksheet worksheet = workbook.Workbook.Worksheets["Properties"];
+                    int currentProperty = 2;
+                    while (true)
+                    {
+                        Property inputProperty = new Property
+                        {
+                            id = worksheet.Cells[currentProperty, 1].Text
+                        };
+                        if (inputProperty.id == "END")
+                        {
+                            break;
+                        }
+
+                        inputProperty.name = worksheet.Cells[currentProperty, 2].Text;
+                        inputProperty.type = worksheet.Cells[currentProperty, 3].Text;
+                        inputProperty.payout = worksheet.Cells[currentProperty, 4].Text;
+                        Console.WriteLine(inputProperty.id + " " + inputProperty.name + " " + inputProperty.type + " " + inputProperty.payout);
+                        currentProperty++;
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error reading configuration file!");
+            }
+        }
+
+        private static void readInput()
+        {
             bool validInput = false;
             while (validInput == false)
             {
@@ -42,9 +84,27 @@ namespace MonopolySimulator
                     validInput = false;
                 }
             }
+        }
 
-            Console.WriteLine("Program terminated...");
-            Console.Read();
+        private static void showTitle()
+        {
+            string title = "+-------------------------------------------------------+" + System.Environment.NewLine +
+                           "|  Monopoly Turn Simulator by Visoiu Mihnea Theodor     |" + System.Environment.NewLine +
+                           "|                                                       |" + System.Environment.NewLine +
+                           "|  This program simulates the turns made by a Monopoly  |" + System.Environment.NewLine +
+                           "|  player in order to find the most visited Property    |" + System.Environment.NewLine +
+                           "|                                                       |" + System.Environment.NewLine +
+                           "|  The developer is not associated with Hasbro Ent.     |" + System.Environment.NewLine +
+                           "+-------------------------------------------------------+";
+            Console.WriteLine(title);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("The board configuration file is located in the same folder");
+            Console.WriteLine("as the executable. It is named boardConfig.xlsx");
+            Console.WriteLine("DO NOT ALTER THE HEADERS IN boardConfig.xlsx");
+            Console.WriteLine("The config file may be opened with a spreadsheet program.");
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
