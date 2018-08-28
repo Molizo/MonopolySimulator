49e0c6130c76bfa4ed7d63f81569ecb002fd4106
Visoiu Mihnea Theodor <visoiu.mihnea@gmail.com>
2018-08-28 15:27:34 +0300

    Added title

--- MonopolySimulator/Program.cs
+++ MonopolySimulator/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MonopolySimulator
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            string title = "+-------------------------------------------------------+" + System.Environment.NewLine +
+                           "|  Monopoly Turn Simulator by Visoiu Mihnea Theodor     |" + System.Environment.NewLine +
+                           "|                                                       |" + System.Environment.NewLine +
+                           "|  This program simulates the turns made by a Monopoly  |" + System.Environment.NewLine +
+                           "|  player in order to find the most visited location    |" + System.Environment.NewLine +
+                           "|                                                       |" + System.Environment.NewLine +
+                           "|  The developer is not associated with Hasbro Ent.     |" + System.Environment.NewLine +
+                           "+-------------------------------------------------------+";
+            Console.WriteLine(title);
+            Console.Read();
         }
     }
 }
