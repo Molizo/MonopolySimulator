67b92c2ec6754684fdcd2bbd61de86c58ae0647b
Visoiu Mihnea Theodor <visoiu.mihnea@gmail.com>
2018-08-28 15:45:25 +0300

    Added input text

--- MonopolySimulator/Program.cs
+++ MonopolySimulator/Program.cs
@@ -6,6 +6,7 @@ namespace MonopolySimulator
     {
         private static void Main(string[] args)
         {
+            ulong turns = 0, games = 0;
             string title = "+-------------------------------------------------------+" + System.Environment.NewLine +
                            "|  Monopoly Turn Simulator by Visoiu Mihnea Theodor     |" + System.Environment.NewLine +
                            "|                                                       |" + System.Environment.NewLine +
@@ -15,6 +16,34 @@ namespace MonopolySimulator
                            "|  The developer is not associated with Hasbro Ent.     |" + System.Environment.NewLine +
                            "+-------------------------------------------------------+";
             Console.WriteLine(title);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("The board configuration file is located in the same folder");
+            Console.WriteLine("as the executable. It is named boardConfig.xlsx");
+            Console.WriteLine("DO NOT ALTER THE HEADERS IN boardConfig.xlsx");
+            Console.WriteLine("The config file may be opened with a spreadsheet program.");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            bool validInput = false;
+            while (validInput == false)
+            {
+                try
+                {
+                    Console.Write("Please enter the number of player turns per game: ");
+                    turns = Convert.ToUInt64(Console.ReadLine());
+                    Console.Write("Please enter the number of games: ");
+                    games = Convert.ToUInt64(Console.ReadLine());
+                    validInput = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input detected. Please try again");
+                    validInput = false;
+                }
+            }
+
+            Console.WriteLine("Program terminated...");
             Console.Read();
         }
     }
